d3e301de8956fed1fe411208b16c6e78ce65085c
Victor Brekenfeld (Drakulix) <drakulix@portingteam.com>
2014-01-22 22:41:54 +0100

    Initial Commit

---
+++ MinecraftServer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MinecraftServer
+//
+//  Created by Victor Brekenfeld on 22.01.14.
+//  Copyright (c) 2014 Victor Brekenfeld. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
