f4043bb210749728c822321968c24775ecc15f89
Victor Brekenfeld (Drakulix) <drakulix@portingteam.com>
2014-01-28 00:07:19 +0100

    Added Xcode Target (otherwise Xcode does not seem to index files. -.- ) Added Debugging Flags

--- build-mac.sh
+++ build-mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-autoreconf
+/opt/local/bin/autoreconf
 ./configure --with-objfw-include-path=./build-mac --with-objfw-lib-path=./build-mac
 make
 install_name_tool -change /Users/Drakulix/Downloads/objfw-HEAD-34192c5/usr/lib/libobjfw.7.dylib @executable_path/libobjfw.dylib objectCraft
