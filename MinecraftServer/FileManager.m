f84e6e1f4960de435d5ffc045b9e4e5a507d2e61
Victor Brekenfeld (Drakulix) <drakulix@portingteam.com>
2014-02-18 17:13:53 +0100

    Threading: Ensure Thread-safety for temporary file creation

--- MinecraftServer/FileManager.m
+++ MinecraftServer/FileManager.m
@@ -9,9 +9,11 @@
 #import "ConfigManager.h"
 #import "FileManager.h"
 #import "AdvancedFile.h"
+#import <dispatch/dispatch.h>
 
 static FileManager *sharedInstance;
 static OFMutableArray *tmpFileArray;
+static dispatch_queue_t fileOperationQueue;
 
 @implementation FileManager
 
@@ -25,6 +27,8 @@ static OFMutableArray *tmpFileArray;
     self = [super init];
     
     @try {
+        if (!fileOperationQueue)
+            fileOperationQueue = dispatch_queue_create("File Operations", NULL);
         if (!tmpFileArray)
             tmpFileArray = [[OFMutableArray alloc] init];
         if (![AdvancedFile directoryExistsAtPath:@"worlds/tmp"]) {
@@ -48,25 +52,35 @@ static OFMutableArray *tmpFileArray;
 }
 
 - (AdvancedFile *)newTmpFile {
-    AdvancedFile *newTmpFile;
     
-    size_t firstNullIndex = [tmpFileArray indexOfObjectIdenticalTo:[OFNull null]];
-    if (firstNullIndex != OF_NOT_FOUND) {
-        newTmpFile = [AdvancedFile fileWithPath:[OFString stringWithFormat:@"worlds/tmp/%d", firstNullIndex] mode:@"a+b"];
-        [tmpFileArray replaceObjectAtIndex:firstNullIndex withObject:newTmpFile];
-    } else {
-        newTmpFile = [AdvancedFile fileWithPath:[OFString stringWithFormat:@"worlds/tmp/%d", [tmpFileArray count]] mode:@"a+b"];
-        [tmpFileArray addObject:newTmpFile];
-    }
+    __block AdvancedFile *newTmpFile;
+    
+    dispatch_sync(fileOperationQueue, ^{
+        
+        size_t firstNullIndex = [tmpFileArray indexOfObjectIdenticalTo:[OFNull null]];
+        if (firstNullIndex != OF_NOT_FOUND) {
+            newTmpFile = [AdvancedFile fileWithPath:[OFString stringWithFormat:@"worlds/tmp/%d", firstNullIndex] mode:@"a+b"];
+            [tmpFileArray replaceObjectAtIndex:firstNullIndex withObject:newTmpFile];
+        } else {
+            newTmpFile = [AdvancedFile fileWithPath:[OFString stringWithFormat:@"worlds/tmp/%d", [tmpFileArray count]] mode:@"a+b"];
+            [tmpFileArray addObject:newTmpFile];
+        }
+        
+    });
     
     return newTmpFile;
 }
 
 - (void)closeTmpFile:(AdvancedFile *)file {
-    [file close];
-    size_t index = [tmpFileArray indexOfObject:file];
-    [AdvancedFile removeItemAtPath:[OFString stringWithFormat:@"worlds/tmp/%d", index]];
-    [tmpFileArray replaceObjectAtIndex:index withObject:[OFNull null]];
+    
+    dispatch_sync(fileOperationQueue, ^{
+        [file close];
+        size_t index = [tmpFileArray indexOfObject:file];
+        [AdvancedFile removeItemAtPath:[OFString stringWithFormat:@"worlds/tmp/%d", index]];
+        [tmpFileArray replaceObjectAtIndex:index withObject:[OFNull null]];
+
+    });
+    
 }
 
 @end
